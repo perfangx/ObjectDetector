a0bafbd92d201364129b276f9985703eb96a646f
Roba gh <rfalghanmi@gmail.com>
2023-05-31 10:41:36 +0300

    Initial Commit

---
+++ ObjectDetector/ObjectDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectorApp.swift
+//  ObjectDetector
+//
+//  Created by roba on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
